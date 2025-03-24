1cc251759bf75345448fda16d960f547c87aca98
Aswarshah <johansmith.bn0753@gmail.com>
2025-03-24 09:02:52 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCWxGEAYSmqUFXhC-5yp-Y8PtGKHQ_oxFM",
+  authDomain: "eid-gift-fe0f6.firebaseapp.com",
+  projectId: "eid-gift-fe0f6",
+  storageBucket: "eid-gift-fe0f6.firebasestorage.app",
+  messagingSenderId: "464963301865",
+  appId: "1:464963301865:web:7ec0ae86fd310e09efdb78"
 };
 
 const app = initializeApp(firebaseConfig);
